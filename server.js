1ff1fe4f0cd066f00bb0763796ce5fae5cd68dd7
Ankur Kunal <ankur.kunal@ibm.com>
2025-10-03 17:48:37 +0530

    Initial commit

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // --- MongoDB Connection ---
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 // --- Expense Schema ---
 const expenseSchema = new mongoose.Schema({
