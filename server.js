5fe62c341e5e163a1d3be591d412967ddb5db537
Ankur Kunal <ankur.kunal@ibm.com>
2025-10-03 17:50:35 +0530

    Initial commit

--- server.js
+++ server.js
@@ -66,5 +66,5 @@ app.get("/expenses", async (req, res) => {
 });
 
 // --- Start Server ---
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
